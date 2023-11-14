7a23aae35a35345ad8c6ef7d9a5c797983772d03
4ref <pricehoolo@gmail.com>
2023-11-14 15:16:48 +0100

    added Entities

---
+++ src/Entity/CiviliteEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Entity;
+
+enum Civilite: string
+{
+    case HOMME = "m";
+    case FEMME = "f";
+}
---
+++ src/Entity/Membre.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MembreRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MembreRepository::class)]
+class Membre
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $pseudo = null;
+
+    #[ORM\Column(length: 60, nullable: true)]
+    private ?string $mdp = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $email = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $status = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $date_enregistrement = null;
+
+    #[ORM\Column(type: "string", length: 255, nullable: true)]
+    private ?string $civilite = null;
+
+    #[ORM\OneToMany(mappedBy: 'membre', targetEntity: Commande::class)]
+    private Collection $vehicule;
+
+    public function __construct()
+    {
+        $this->vehicule = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPseudo(): ?string
+    {
+        return $this->pseudo;
+    }
+
+    public function setPseudo(?string $pseudo): static
+    {
+        $this->pseudo = $pseudo;
+
+        return $this;
+    }
+
+    public function getMdp(): ?string
+    {
+        return $this->mdp;
+    }
+
+    public function setMdp(?string $mdp): static
+    {
+        $this->mdp = $mdp;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): static
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getStatus(): ?int
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?int $status): static
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getDateEnregistrement(): ?\DateTimeInterface
+    {
+        return $this->date_enregistrement;
+    }
+
+    public function setDateEnregistrement(?\DateTimeInterface $date_enregistrement): static
+    {
+        $this->date_enregistrement = $date_enregistrement;
+
+        return $this;
+    }
+
+    public function getCivilite(): ?string
+    {
+        return $this->civilite;
+    }
+
+    public function setCivilite(?string $civilite): static
+    {
+        $this->civilite = $civilite;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Commande>
+     */
+    public function getVehicule(): Collection
+    {
+        return $this->vehicule;
+    }
+
+    public function addVehicule(Commande $vehicule): static
+    {
+        if (!$this->vehicule->contains($vehicule)) {
+            $this->vehicule->add($vehicule);
+            $vehicule->setMembre($this);
+        }
+
+        return $this;
+    }
+
+    public function removeVehicule(Commande $vehicule): static
+    {
+        if ($this->vehicule->removeElement($vehicule)) {
+            // set the owning side to null (unless already changed)
+            if ($vehicule->getMembre() === $this) {
+                $vehicule->setMembre(null);
+            }
+        }
+
+        return $this;
+    }
+}
