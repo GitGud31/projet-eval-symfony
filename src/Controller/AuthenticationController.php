d2e025d99b1c67bf385c28f99d21dbe2e1790eee
4ref <pricehoolo@gmail.com>
2023-11-15 13:31:42 +0100

    addedl: Authentication UI

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Form\AuthenticationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,8 +12,13 @@ class AuthenticationController extends AbstractController
     #[Route('/connexion', name: 'connexion')]
     public function authentication(): Response
     {
+        $formValue = $this->createform(AuthenticationType::class);
+
+        # dd($formValue);
+
         return $this->render('authentication/index.html.twig', [
             'controller_name' => 'AuthenticationController',
+            'form_value' => $formValue,
         ]);
     }
 }
---
+++ src/Form/AuthenticationType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Membre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class AuthenticationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo')
+            ->add('mdp', PasswordType::class)
+            //->add('nom')
+            //->add('prenom')
+            //->add('email')
+            //->add('status')
+            //->add('date_enregistrement')
+            //->add('civilite')
+            ->add('Connexion', SubmitType::class)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Membre::class,
+        ]);
+    }
+}
