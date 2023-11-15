61694cfab80bf65335eb167d61b649da86ad6928
4ref <pricehoolo@gmail.com>
2023-11-15 14:36:07 +0100

    added: Signup UI

--- src/Controller/AuthenticationController.php
+++ src/Controller/AuthenticationController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Form\AuthenticationType;
+use App\Form\SignupType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,13 +11,24 @@ use Symfony\Component\Routing\Annotation\Route;
 class AuthenticationController extends AbstractController
 {
     #[Route('/connexion', name: 'connexion')]
-    public function authentication(): Response
+    public function connexion(): Response
     {
         $formValue = $this->createform(AuthenticationType::class);
 
         # dd($formValue);
 
         return $this->render('authentication/index.html.twig', [
+            'controller_name' => 'AuthenticationController',
+            'form_value' => $formValue,
+        ]);
+    }
+
+    #[Route('/signup', name: 'signup')]
+    public function signup(): Response
+    {
+        $formValue = $this->createForm(SignupType::class);
+
+        return $this->render('sign_up/index.html.twig', [
             'controller_name' => 'AuthenticationController',
             'form_value' => $formValue,
         ]);
