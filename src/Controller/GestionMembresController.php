36879de48c06d7f7978e8e115840fc1ff12f3df1
4ref <pricehoolo@gmail.com>
2023-11-17 15:02:34 +0100

    Added: Add new membre

--- src/Controller/GestionMembresController.php
+++ src/Controller/GestionMembresController.php
@@ -3,30 +3,53 @@
 namespace App\Controller;
 
 use App\Entity\Membre;
+use App\Form\AddMembreType;
 use App\Form\EditProfileType;
+use App\Form\SignupType;
 use App\Repository\CommandeRepository;
 use App\Repository\MembreRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GestionMembresController extends AbstractController
 {
     #[Route('/gestion/membres', name: 'gestion_membres')]
-    public function index(MembreRepository $membreRepository): Response
+    public function index(
+        Request                     $request,
+        UserPasswordHasherInterface $passwordHasher,
+        EntityManagerInterface      $entityManager,
+        MembreRepository            $membreRepository): Response
     {
-
         $membres = $membreRepository->findAll();
+
+        # A new membre is automatically registered with DateTime Now.
+        $membre = new Membre();
+        $membre->setDateEnregistrement(new \DateTime());
+
+        $formValue = $this->createForm(AddMembreType::class, $membre);
+        $formValue->handleRequest($request);
+
+        if ($formValue->isSubmitted() && $formValue->isValid()) {
+            $membre->setMdp($passwordHasher->hashPassword($membre, $membre->getMdp()));
+
+            $entityManager->persist($membre);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('gestion_membres');
+        }
 
         return $this->render('gestion_membres/index.html.twig', [
             'controller_name' => 'GestionMembresController',
             'membres' => $membres,
+            'form_value' => $formValue,
         ]);
     }
 
-    #[Route('/member/delete/{id}', name: 'member_delete')]
+    #[Route('/gestion/member/delete/{id}', name: 'member_delete')]
     public function delete(Membre             $membre,
                            MembreRepository   $membreRepository,
                            CommandeRepository $commandeRepository): Response
@@ -39,7 +62,7 @@ class GestionMembresController extends AbstractController
         return $this->redirectToRoute('gestion_membres');
     }
 
-    #[Route('/member/edit/{id}', name: 'edit_membre')]
+    #[Route('/gestion/membre/edit/{id}', name: 'edit_membre')]
     public function edit(Membre                 $membre,
                          EntityManagerInterface $entityManager,
                          Request                $request)
